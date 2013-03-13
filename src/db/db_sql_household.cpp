d8123798563dc37cd1a11f7b0e81cf1a22e71f42
Vincent Rabaud <vrabaud@willowgarage.com>
2013-03-12 18:03:05 -0700

    Rebase from 'debian/groovy/object_recognition_tabletop'

--- src/db/db_sql_household.cpp
+++ src/db/db_sql_household.cpp
@@ -34,42 +34,51 @@
  */
 
 #include <sstream>
+#include <object_recognition_core/db/db.h>
 #include <object_recognition_tabletop/household.h>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ObjectDbSqlHousehold::ObjectDbSqlHousehold()
 {
-  ObjectDbParametersRaw parameters = default_raw_parameters();
-  // Create the DB object
-  db_ = boost::shared_ptr<household_objects_database::ObjectsDatabase>(
-      new household_objects_database::ObjectsDatabase(parameters["host"].get_str(), parameters["port"].get_str(),
-                                                      parameters["user"].get_str(), parameters["password"].get_str(),
-                                                      parameters["name"].get_str()));
+  object_recognition_core::db::ObjectDbParameters parameters(default_raw_parameters());
+  this->set_parameters(parameters);
 }
 
-ObjectDbSqlHousehold::ObjectDbSqlHousehold(ObjectDbParametersRaw & in_parameters)
+void
+ObjectDbSqlHousehold::set_parameters(ObjectDbParameters & in_parameters)
 {
-  ObjectDbParametersRaw parameters = default_raw_parameters();
+  ObjectDbParametersRaw parameters = in_parameters.raw();
   // Read the parameters
   for (ObjectDbParametersRaw::const_iterator iter = parameters.begin(), end = parameters.end(); iter != end; ++iter)
   {
-    if(iter->first == "type")
+    if (iter->first == "type")
       continue;
 
-    ObjectDbParametersRaw::const_iterator val = in_parameters.find(iter->first);
-    if (val == in_parameters.end())
-      std::cerr << "The db parameters do not contain the field \"" << iter->first << "\". Using the default: \""
-                << iter->second.get_str() << "\"" << std::endl;
-    else
-      parameters[iter->first] = val->second.get_str();
+    ObjectDbParametersRaw::const_iterator val = parameters.find(iter->first);
+    if (val == parameters.end())
+      std::cerr << "The db parameters do not contain the field \""
+          << iter->first << "\". Using the default: \""
+          << iter->second.get_str() << "\"" << std::endl;
+    else {
+      if ((iter->first == "port") && (val->second.type() == or_json::int_type)) {
+        std::stringstream ss;
+        ss << val->second.get_int();
+        parameters[iter->first] = ss.str();
+      } else {
+        if (val->second.type() != or_json::str_type)
+          throw std::runtime_error(
+              std::string("Key \"") + val->first
+                  + std::string("\" needs to be a string"));
+        parameters[iter->first] = val->second.get_str();
+      }
+    }
   }
-  in_parameters = parameters;
+  in_parameters = object_recognition_core::db::ObjectDbParameters(parameters);
+  parameters_ = in_parameters;
 
-
-// Create the DB object
-  db_ = boost::shared_ptr<household_objects_database::ObjectsDatabase>(
-      new household_objects_database::ObjectsDatabase(parameters.at("host").get_str(), parameters.at("port").get_str(),
+  // Create the DB object
+  db_.reset(new household_objects_database::ObjectsDatabase(parameters.at("host").get_str(), parameters.at("port").get_str(),
                                                       parameters.at("user").get_str(),
                                                       parameters.at("password").get_str(),
                                                       parameters.at("name").get_str()));
@@ -124,18 +133,6 @@ ObjectDbSqlHousehold::get_attachment_stream(const DocumentId & document_id, cons
 }
 
 void
-ObjectDbSqlHousehold::GetObjectRevisionId(DocumentId& document_id, RevisionId & revision_id)
-{
-  throw std::runtime_error("Function not implemented in the SQL household DB.");
-}
-
-void
-ObjectDbSqlHousehold::GetRevisionId(RevisionId & revision_id)
-{
-  throw std::runtime_error("Function not implemented in the SQL household DB.");
-}
-
-void
 ObjectDbSqlHousehold::Delete(const ObjectId & id)
 {
   throw std::runtime_error("Function not implemented in the SQL household DB.");
@@ -149,7 +146,78 @@ ObjectDbSqlHousehold::QueryView(const object_recognition_core::db::View & view, 
   {
     case object_recognition_core::db::View::VIEW_MODEL_WHERE_OBJECT_ID_AND_MODEL_TYPE:
     {
-      throw std::runtime_error("Function not implemented in the SQL household DB.");
+      object_recognition_core::db::View::Key key;
+      std::string options;
+      if (view.key(key))
+      {
+        // Load the mesh from the DB
+        household_objects_database::DatabaseMesh mesh;
+        db_->getScaledModelMesh(atoi(key.get_str().c_str()), mesh);
+
+        std::stringstream stream;
+        // Write the mesh to a stream according to the specs at http://en.wikipedia.org/wiki/STL_%28file_format%29
+        int sizeof_uc = 1;
+        int sizeof_us = 2;
+        int sizeof_ui = 4;
+        int sizeof_f = 4;
+        // Write the random 80 character header
+        for (unsigned char i = 0; i < 80; ++i)
+          stream.write(reinterpret_cast<char*>(&i), sizeof_uc);
+        unsigned int n_triangles = mesh.triangles_.data().size() / 3;
+        stream.write(reinterpret_cast<char*>(&n_triangles), sizeof_ui);
+
+        unsigned short zero_us = 0;
+        for (size_t i = 0; i < n_triangles; ++i) {
+          float x1[3];
+          float x2[3];
+          float x3[3];
+          float X1[3], X2[3];
+          // For each coordinate of the vertex
+          for (char k = 0; k < 3; ++k) {
+            x1[k] = mesh.vertices_.data().at(
+                3 * mesh.triangles_.data().at(3 * i + 0) + k);
+            x2[k] = mesh.vertices_.data().at(
+                3 * mesh.triangles_.data().at(3 * i + 1) + k);
+            x3[k] = mesh.vertices_.data().at(
+                3 * mesh.triangles_.data().at(3 * i + 2) + k);
+            X1[k] = x2[k]-x1[k];
+            X2[k] = x3[k]-x1[k];
+          }
+          float normal[3];
+          normal[0] = X1[1] * X2[2] - X1[2] * X2[1];
+          normal[1] = X2[0] * X1[2] - X2[2] * X1[0];
+          normal[2] = X1[0] * X2[1] - X1[1] * X2[0];
+          float norm = std::sqrt(
+              normal[0] * normal[0] + normal[1] * normal[1]
+                  + normal[2] * normal[2]);
+          for (char k = 0; k < 3; ++k)
+            normal[k] = 0*normal[k] / norm;
+          // Write the normal
+          stream.write(reinterpret_cast<char*>(&normal[0]), sizeof_f).write(
+              reinterpret_cast<char*>(&normal[1]), sizeof_f).write(
+              reinterpret_cast<char*>(&normal[2]), sizeof_f);
+          // Write the 3 vertices
+          stream.write(reinterpret_cast<char*>(&x1[0]), sizeof_f).write(
+              reinterpret_cast<char*>(&x1[1]), sizeof_f).write(
+              reinterpret_cast<char*>(&x1[2]), sizeof_f);
+          stream.write(reinterpret_cast<char*>(&x2[0]), sizeof_f).write(
+              reinterpret_cast<char*>(&x2[1]), sizeof_f).write(
+              reinterpret_cast<char*>(&x2[2]), sizeof_f);
+          stream.write(reinterpret_cast<char*>(&x3[0]), sizeof_f).write(
+              reinterpret_cast<char*>(&x3[1]), sizeof_f).write(
+              reinterpret_cast<char*>(&x3[2]), sizeof_f);
+          // Write the attribute
+          stream.write(reinterpret_cast<char*>(&zero_us), sizeof_us);
+        }
+
+        // Create the view element
+        ViewElement view_element("", key.get_str());
+        view_element.set_field("name", key.get_str());
+        view_element.set_attachment_stream("mesh", stream);
+        view_elements.push_back(view_element);
+      }
+      else
+        throw std::runtime_error("Function not implemented in the SQL household DB.");
       break;
     }
     case object_recognition_core::db::View::VIEW_OBJECT_INFO_WHERE_OBJECT_ID:
@@ -158,11 +226,9 @@ ObjectDbSqlHousehold::QueryView(const object_recognition_core::db::View & view, 
       std::string options;
       if (view.key(key))
       {
-        // Get the information from the different tables
-        BOOST_FOREACH(ViewElement & view_element, view_elements)
-        {
-
-        }
+        ViewElement view_element("", key.get_str());
+        view_element.set_field("name", key.get_str());
+        view_elements.push_back(view_element);
       }
       else
         throw std::runtime_error("Function not implemented in the SQL household DB.");
@@ -213,8 +279,6 @@ ObjectDbSqlHousehold::DeleteCollection(const CollectionName &collection)
   throw std::runtime_error("Function not implemented in the SQL household DB.");
 }
 
-void
-ObjectDbSqlHousehold::upload_json(const or_json::mObject &params, const std::string& url, const std::string& request)
-{
-  throw std::runtime_error("Function not implemented in the SQL household DB.");
-}
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(ObjectDbSqlHousehold, object_recognition_core::db::ObjectDb)
